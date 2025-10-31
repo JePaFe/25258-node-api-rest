930f5d2d42d01dd37122a0e7678f4534e6b9be01
Jean Paul Ferreira <jeanpaul@jepafe.net>
2025-10-30 21:52:04 -0300

    Clase 10 - API Folders

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola Node.js!!!");
+});
+
+import productsRouter from "./src/routes/products.router.js";
+app.use("/api/products", productsRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
